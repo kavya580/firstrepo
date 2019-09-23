0d44b750d2853d5f77709f5b9840c326e26417b4
Anil <Anil@DESKTOP-8ADDD0H>
2019-09-23 15:14:22 +0530

    initial commit

---
+++ GitProject/src/com/faith/test/Main.java
@@ -0,0 +1,15 @@
+package com.faith.test;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData objSalesData = new SalesData();
+		System.out.println("Hello Ust Guys!! Git Repo");
+		displayGreetings();
+		objSalesData.display();
+
+	}
+
+	public static void displayGreetings() {
+		System.out.println("Welcome to sales Repo App");
+	}
+}
---
+++ GitProject/src/com/faith/test/SalesData.java
@@ -0,0 +1,7 @@
+package com.faith.test;
+
+public class SalesData {
+public void display(){
+	System.out.println("display function is working");
+}
+}
